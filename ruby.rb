e4739f7c75a235ca8553d2d8345bd56aa70ef595
RobertSarsoza <sarsoza@gmail.com>
2024-01-22 20:37:10 -0800

    almost done

--- ruby.rb
+++ ruby.rb
@@ -7,4 +7,25 @@
 # Add comments to your code explaining your approach
 
 class Bike
+    attr_accessor :wheels, :current_speed
+  attr_reader :model
+
+  def initialize(model)
+    @model = model
+    @wheels = 2
+    @current_speed = 0
+  end
+
+  def pedal_faster(speed_increase)
+    @current_speed += speed_increase
+  end
+
+  def brake(speed_decrease)
+    @current_speed = [0, @current_speed - speed_decrease].max
+  end
+
+  def bike_info
+    "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph."
+  end
+
 end
